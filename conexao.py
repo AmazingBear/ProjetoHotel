5752d159b9437b94a0637f5c9badc75dedaa9300
AmazingBear <vitord3152002@gmail.com>
2021-07-12 01:29:47 -0300

    Mudanças na parte visual

--- conexao.py
+++ conexao.py
@@ -9,3 +9,10 @@ def conectar():
         cursorclass=pymysql.cursors.DictCursor,
     )
     return conn
+
+def delete_cliente(self, id):
+    with self.conn.cursor() as cursor:
+        query = 'delete from tbdetalhematricula where id = %s'
+        cursor.execute(query, (id))
+        self.conn.commit()
+        self.conn.close()
