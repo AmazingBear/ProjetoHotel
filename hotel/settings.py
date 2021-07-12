2405301cf310e6b88acba610e8b35cc2b6e76a41
AmazingBear <vitord3152002@gmail.com>
2021-07-11 23:45:37 -0300

    Commit do projeto com cadastro funcional, mas não com 100% da estilização

--- hotel/settings.py
+++ hotel/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9mn=kr$4_6ut5(c0)^u3pc52qch0!5!&jxicu36@=^x@)9pa8i'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
